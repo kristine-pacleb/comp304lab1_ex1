cc85533c88bee97e7547b28dfc70d2a13db48424
kristinepacleb <codewtihtine@gmail.com>
2020-09-26 11:05:57 -0400

    Toast with dynamic Resource strings.xml

--- app/src/main/java/com/example/marykristinepacleb_comp304lab1_ex1/MainActivity.java
+++ app/src/main/java/com/example/marykristinepacleb_comp304lab1_ex1/MainActivity.java
@@ -10,52 +10,51 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
     String tag = "Lifecycle";
     FragmentSecond fragmentSecond;
+    TextView t;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, R.string.onCreate, Toast.LENGTH_LONG).show();
     }
 
         public void onStart()
         {
             super.onStart();
-            Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
-      }
+            Toast.makeText(this, R.string.onStart, Toast.LENGTH_LONG).show();
+        }
 
         public void onRestart()
         {
             super.onRestart();
-            Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.onRestart, Toast.LENGTH_LONG).show();
         }
 
         public void onResume()
         {
             super.onResume();
-            Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.onResume, Toast.LENGTH_LONG).show();
         }
         public void onPause()
         {
             super.onPause();
-            Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.onPause, Toast.LENGTH_LONG).show();
         }
         //
         public void onStop()
         {
             super.onStop();
-            Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.onStop, Toast.LENGTH_LONG).show();
         }
         public void onDestroy()
         {
             super.onDestroy();
-            Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.onDestroy, Toast.LENGTH_LONG).show();
         }
-
-
-
 }
