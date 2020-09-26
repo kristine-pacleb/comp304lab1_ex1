4b1de23a41d3cfdcdd8ad57dfec75e6532d8e749
kristinepacleb <codewtihtine@gmail.com>
2020-09-26 01:12:05 -0400

    All requirements are working now.

---
+++ app/src/main/java/com/example/marykristinepacleb_comp304lab1_ex1/FragmentSecond.java
@@ -0,0 +1,19 @@
+package com.example.marykristinepacleb_comp304lab1_ex1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentSecond extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+}
